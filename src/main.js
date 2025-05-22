033c88f94d098e587d3dd2cfc9aee28343633201
ZalaNihir <zalanihir8866@gmail.com>
2025-05-22 22:40:13 +0530

    install the analystics & optimize the code

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import VueSmoothScroll from 'vue3-smooth-scroll'
+import { inject } from '@vercel/analytics';
+inject();
 
 const app = createApp(App)
+
 app.use(VueSmoothScroll)
+
 app.mount('#app')
